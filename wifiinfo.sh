e187e170074db9a8f58224fb4a78c30bb478b1fd
Daniil Pechersky <cdtt38.pecherskyD@yandex.ru>
2024-02-20 10:44:43 +0800

    Исправление скрипта

--- wifiinfo.sh
+++ wifiinfo.sh
@@ -2,15 +2,10 @@
 
 wifiname=$2
 filename=$1
-status=$(nmcli device wifi rescan)
-if test "$status"=" "
+status=$(nmcli dev wifi rescan)
+if test "$wifiname"!=""
 then
-    echo "No wifi device found" > $filename
+    nmcli dev wifi list|grep "$wifiname">>$filename
 else
-    if test "$wifiname"!=""
-    then
-        nmcli dev wifi list|grep "$wifiname">>$filename
-    else
-        nmcli dev wifi list>>$filename
-    fi
+    nmcli dev wifi list>>$filename
 fi
